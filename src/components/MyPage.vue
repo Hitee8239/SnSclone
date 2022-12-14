4f4953da9a61bec76235b865326ae15eeffa4ace
hitee <laby8239@gmail.com>
2022-12-14 22:55:55 +0900

    검색기능 추가

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding: 10px">
     <h4>팔로워</h4>
-    <input placeholder="?" />
+    <input placeholder="?" @input="search($event.target.value)" />
     <div class="post-header" v-for="(a, i) in follower" :key="i">
       <div class="profile" :style="`background-image:url(${a.image})`"></div>
       <span class="profile-name">{{ a.name }}</span>
@@ -15,17 +15,25 @@ import axios from "axios";
 
 export default {
   name: "myPage",
-  setup() {
+  setup(){
     let follower = ref([]);
+    let followerOriginal = ref([]);
 
-    onMounted(() => {
-      axios.get("/follower.json").then((a) => {
+    onMounted(()=>{
+      axios.get('/follower.json').then((a)=>{
         follower.value = a.data;
-      });
+        followerOriginal.value = [...a.data];
+      })
     });
 
-    return { follower };
-  },
+    function search(검색어){
+      let newFollower = followerOriginal.value.filter((a)=>{
+        return a.name.indexOf(검색어) != -1
+      });
+      follower.value = [...newFollower]
+    }
+    return {follower, search}
+  }, 
 };
 </script>
 
